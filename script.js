b188796df7ef5b530328f219015ab8470090c477
Ates06 <ates.qurbanov06@gmail.com>
2023-09-26 22:25:07 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const dark = document.getElementById("dark");
+window.onmousemove = function (e) {
+    const x = e.x / window.innerWidth * 100;
+    const y = e.y / window.innerHeight * 100;
+    dark.style.background = `radial-gradient(circle at ${x}% ${y}%, transparent, rgb(0, 0, 0, .9) 150px, black)`};
